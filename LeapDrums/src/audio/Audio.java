410468de40f5fea5ad0d298140a18eff4e4d612d
shudas <shudas@ADDC-DL106.adsroot.itcs.umich.edu>
2013-11-27 14:28:52 -0500

    Separate audio class and tried to do relative path import for samples

--- LeapDrums/src/audio/Audio.java
+++ LeapDrums/src/audio/Audio.java
@@ -1,11 +1,54 @@
 package audio;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.*;
+
 import javax.sound.sampled.*;
 
 public class Audio {
+    private Clip clip;
+    public Audio(String fileName) {
+        // specify the sound to play
+        // (assuming the sound can be played by the audio system)
+        // from a wave File
+        try {
+            AudioInputStream sound = (AudioInputStream) this.getClass().getResourceAsStream(fileName);
+            // load the sound into memory (a Clip)
+            clip = AudioSystem.getClip();
+            clip.open(sound);
+        }
+        catch (java.net.MalformedURLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Sound: Malformed URL: " + e);
+        }
+//        catch (UnsupportedAudioFileException e) {
+//            e.printStackTrace();
+//            throw new RuntimeException("Sound: Unsupported Audio File: " + e);
+//        }
+        catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Sound: Input/Output Error: " + e);
+        }
+        catch (LineUnavailableException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Sound: Line Unavailable Exception Error: " + e);
+        }
+        catch (Exception e){
+        	e.printStackTrace();
+            throw new RuntimeException("Sound: file not found: " + fileName);
+        }
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello Git World!");
-	}
-
-}
+    // play, stop, loop the sound clip
+    }
+    public void play(){
+        clip.setFramePosition(0);  // Must always rewind!
+        clip.start();
+    }
+    public void loop(){
+        clip.loop(Clip.LOOP_CONTINUOUSLY);
+    }
+    public void stop(){
+            clip.stop();
+        }
+    }
---
+++ LeapDrums/src/audio/TestMain.java
@@ -0,0 +1,11 @@
+package audio;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Audio audio1 = new Audio("hat01.aif");
+		audio1.play();
+	}
+
+}
