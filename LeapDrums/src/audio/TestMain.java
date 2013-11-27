9fdd164d0a414546ad7983b0ad600da7208e7df6
shudas <shudas@ADDC-DL106.adsroot.itcs.umich.edu>
2013-11-27 16:01:33 -0500

    A sound is being played!!!

--- LeapDrums/src/audio/TestMain.java
+++ LeapDrums/src/audio/TestMain.java
@@ -1,11 +1,17 @@
 package audio;
 
+import javax.swing.JFrame;
+
 public class TestMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Open frame so that program doesnt close immediately after running
+		JFrame frame = new JFrame();
+        frame.setSize(300,300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+		
 		Audio audio1 = new Audio("snare01.wav");
-		audio1.play();
 	}
 
 }
