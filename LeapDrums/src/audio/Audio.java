e96f15c7ba6249398a4d573b785a6e5a7d085eaa
shudas <shudas@ADDC-DL106.adsroot.itcs.umich.edu>
2013-11-27 15:10:49 -0500

    Trying to get relative paths to work

--- LeapDrums/src/audio/Audio.java
+++ LeapDrums/src/audio/Audio.java
@@ -3,6 +3,7 @@ package audio;
 import java.io.File;
 import java.io.IOException;
 import java.net.*;
+import java.nio.file.*;
 
 import javax.sound.sampled.*;
 
@@ -13,31 +14,18 @@ public class Audio {
         // (assuming the sound can be played by the audio system)
         // from a wave File
         try {
-            AudioInputStream sound = (AudioInputStream) this.getClass().getResourceAsStream(fileName);
+            String absPath = new File("").getAbsolutePath() + "\\samples\\" + fileName;
+            System.out.println(absPath);
+            AudioInputStream sound = (AudioInputStream) this.getClass().getResourceAsStream(absPath);
             // load the sound into memory (a Clip)
             clip = AudioSystem.getClip();
             clip.open(sound);
         }
-        catch (java.net.MalformedURLException e) {
+        catch (Exception e) {
             e.printStackTrace();
-            throw new RuntimeException("Sound: Malformed URL: " + e);
+            throw new RuntimeException(e);
         }
-//        catch (UnsupportedAudioFileException e) {
-//            e.printStackTrace();
-//            throw new RuntimeException("Sound: Unsupported Audio File: " + e);
-//        }
-        catch (IOException e) {
-            e.printStackTrace();
-            throw new RuntimeException("Sound: Input/Output Error: " + e);
-        }
-        catch (LineUnavailableException e) {
-            e.printStackTrace();
-            throw new RuntimeException("Sound: Line Unavailable Exception Error: " + e);
-        }
-        catch (Exception e){
-        	e.printStackTrace();
-            throw new RuntimeException("Sound: file not found: " + fileName);
-        }
+        
 
     // play, stop, loop the sound clip
     }
--- LeapDrums/src/audio/TestMain.java
+++ LeapDrums/src/audio/TestMain.java
@@ -4,8 +4,7 @@ public class TestMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Audio audio1 = new Audio("hat01.aif");
-		audio1.play();
+		Audio audio1 = new Audio("kick01.aif");
 	}
 
 }
