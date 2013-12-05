3e360817d7606c21cb28f9ba8967879b3e816ab5
shudas <shudas@umich.edu>
2013-12-05 18:56:20 -0500

    Added basic UI

--- LeapDrums/src/Main/TestMain.java
+++ LeapDrums/src/Main/TestMain.java
@@ -1,17 +1,37 @@
 package Main;
 
+import java.awt.EventQueue;
+
 import javax.swing.JFrame;
+import javax.swing.UIManager;
+
 import ui.*;
 import audio.Audio;
 
 public class TestMain {
 
+	/**
+	 * Launch the application.
+	 */
 	public static void main(String[] args) {
-		// Open frame so that program doesnt close immediately after running
-		UserInterface ui = new UserInterface();
+		try {
+            // Set cross-platform Java L&F (also called "Metal")
+	        UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+	    }
+		catch( Exception e){
+			throw new RuntimeException("Could not set look and feel");
+		}
 		
-		Audio audio1 = new Audio("snare01.wav");
-		audio1.play();
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					mainUI window = new mainUI();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 
 }
