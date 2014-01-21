b5c879f73a7c6d1824f46aa7b685835ee3b3328d
shudas <shudas@ADDC-DL112.adsroot.itcs.umich.edu>
2014-01-21 15:36:43 -0500

    Deleted UI package so I can work only on Audio related stuff

--- LeapDrums/src/Main/TestMain.java
+++ LeapDrums/src/Main/TestMain.java
@@ -5,7 +5,6 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.UIManager;
 
-import ui.*;
 import audio.Audio;
 
 public class TestMain {
@@ -14,24 +13,7 @@ public class TestMain {
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-		try {
-            // Set cross-platform Java L&F (also called "Metal")
-	        UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
-	    }
-		catch( Exception e){
-			throw new RuntimeException("Could not set look and feel");
-		}
 		
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					mainUI window = new mainUI();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
 	}
 
 }
